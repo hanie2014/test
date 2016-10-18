f989c5ab71ec9bb92f0c59e261fd88748ff71df3
abdecph <abdecph@gmail.com>
2016-10-18 20:05:47 +0100

    Add files via upload

---
+++ sale_qty/__openerp__.py
@@ -0,0 +1,10 @@
+{
+	'name': 'Sale',
+	'version': 1.1,
+	'category': 'Sales Management',
+	'description': 'Sale',
+	'depends': ['sale', 'web'],
+	'installable': True,
+	'auto_install': False,
+	'application': True,
+}
