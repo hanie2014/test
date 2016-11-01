af858535dd61e2d4d9feca1855e6cae2cf8bb500
Hanie <abde2011@ymail.com>
2016-11-01 21:16:09 +0100

    Update __openerp__.py

--- sale_qty/__openerp__.py
+++ sale_qty/__openerp__.py
@@ -2,6 +2,8 @@
 	'name': 'Sale',
 	'version': '1.1',
 	'author': 'hanie',
+	'price': 4.00,
+	'currency': 'USD'
 	'category': 'Sales Management',
 	'description': 'Sale model',
 	'depends': ['sale', 'web'],
