0ee744178592fb736e1e16b0fded96df7fa4772b
Hanie <abde2011@ymail.com>
2016-11-02 21:52:03 +0100

    Update __openerp__.py

--- sale_qty/__openerp__.py
+++ sale_qty/__openerp__.py
@@ -2,8 +2,6 @@
 	'name': 'Sale',
 	'version': '1.1',
 	'author': 'hanie',
-	'price': 2.00,
-	'currency': 'USD',
 	'category': 'Sales Management',
 	'description': 'Sale model calculate qty units from packaging',
 	'depends': ['sale', 'web'],
