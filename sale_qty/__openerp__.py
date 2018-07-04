81c19788c56d31cd79035148c78b110f28bdd9ba
Hanie <abde2011@ymail.com>
2018-07-04 15:39:56 +0100

    Update __openerp__.py

--- sale_qty/__openerp__.py
+++ sale_qty/__openerp__.py
@@ -2,7 +2,7 @@
 	'name': 'Sale',
 	'version': 1.1,
 	'category': 'Sales Management',
-	'description': 'Sale',
+	'description': 'Add Calculate QTY From Packaging. In Sale Order ',
 	'depends': ['sale', 'web'],
 	'installable': True,
 	'auto_install': False,
